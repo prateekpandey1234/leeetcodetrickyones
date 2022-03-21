85436ff329054085c25140b8ba81cf525f1a9da2
prateekpandey1234 <prateek775999@gmail.com>
2022-03-21 06:36:02 +0530

    day 21

--- src/Practice.java
+++ src/Practice.java
@@ -17,5 +17,23 @@ class TreeNode {
 
 
 public class Practice {
-
+    public int sumRootToLeaf(TreeNode root) {
+        List<Integer> ans2 = new ArrayList<>();
+        ans2.set(0,0);
+        String ans3 ="";
+        return solve(root,ans3,ans2).get(0);
+    }
+    public List<Integer> solve(TreeNode root,String ans,List<Integer> ans1){
+        if(root==null){
+            return ans1;
+        }
+        if(root.right==null && root.left==null){
+            ans1.set(0,ans1.get(0)+Integer.parseInt(ans,2));
+            return ans1;
+        }
+        ans+=String.valueOf(root.val);
+        ans1=solve(root.right,ans,ans1);
+        ans1=solve(root.left,ans,ans1);
+        return ans1;
+    }
 }
