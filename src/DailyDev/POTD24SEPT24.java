a213ef3f0b882393511d3e4429cca4c69ec10bf5
prateekpandey1234 <prateek775999@gmail.com>
2024-09-24 08:51:24 +0530

    1.Trie,HashSet

---
+++ src/DailyDev/POTD24SEPT24.java
@@ -0,0 +1,27 @@
+package DailyDev;
+
+public class POTD24SEPT24 {
+    // needed bit of hint , but did good implementation
+    class TrieNode{
+        TrieNode[] childs;
+        TrieNode(){
+            this.childs=new TrieNode[10];
+        }
+    }
+    class Trie{
+        TrieNode root ;
+        Trie(){
+            this.root= new TrieNode();
+        }
+        public void insert(String str){
+            TrieNode temp = root;
+            for(char c:str.toCharArray()){
+                if(temp.childs[c-'0']==null){
+                    temp.childs[c-'0']=new TrieNode();
+                }
+                temp=temp.childs[c-'0'];
+            }
+        }
+    }
+    // another approach is using hashset but it seems brute force
+}
