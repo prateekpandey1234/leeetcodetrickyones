3ad923e2f8e6afa64e91a72348e9ccecd3e65daa
prateekpandey1234 <prateek775999@gmail.com>
2024-10-07 13:38:20 +0530

    1.Also did (Oct 6) question

--- src/DailyDev/October/POTD7OCT2024.java
+++ src/DailyDev/October/POTD7OCT2024.java
@@ -1,5 +1,8 @@
 package DailyDev.October;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.Stack;
 
 public class POTD7OCT2024 {
@@ -21,5 +24,62 @@ public class POTD7OCT2024 {
             else stck.clear();
         }
         return ans;
+    }
+}
+class POTD6_OCT2024 {
+    // solving previous day question
+    class Solution {
+
+        public boolean areSentencesSimilar(String s1, String s2) {
+            // Split the words in sentences and store it in a string array.
+            String[] s1Words = s1.split(" "), s2Words = s2.split(" ");
+            int start = 0, ends1 = s1Words.length - 1, ends2 =
+                    s2Words.length - 1, s1WordsLength = s1Words.length, s2WordsLength =
+                    s2Words.length;
+
+            // If words in s1 are more than s2, swap them and return the answer.
+            if (s1WordsLength > s2WordsLength) {
+                return areSentencesSimilar(s2, s1);
+            }
+
+            // Find the maximum words matching from the beginning.
+            while (start < s1WordsLength && s1Words[start].equals(s2Words[start])) {
+                ++start;
+            }
+            // Find the maximum words matching in the end.
+            while (ends1 >= 0 && s1Words[ends1].equals(s2Words[ends2])) {
+                --ends1;
+                --ends2;
+            }
+
+            // If i reaches the end of the array, then we return true.
+            return ends1 < start;
+        }
+    }
+    class Solution2 {
+
+        public boolean areSentencesSimilar(String s1, String s2) {
+            Deque<String> deque1 = new ArrayDeque<>(Arrays.asList(s1.split(" ")));
+            Deque<String> deque2 = new ArrayDeque<>(Arrays.asList(s2.split(" ")));
+            //Compare the prefixes or beginning of the strings.
+            while (
+                    !deque1.isEmpty() &&
+                            !deque2.isEmpty() &&
+                            deque1.peek().equals(deque2.peek())
+            ) {
+                deque1.poll();
+                deque2.poll();
+            }
+            // Compare the suffixes or ending of the strings.
+            while (
+                    !deque1.isEmpty() &&
+                            !deque2.isEmpty() &&
+                            deque1.peekLast().equals(deque2.peekLast())
+            ) {
+                deque1.pollLast();
+                deque2.pollLast();
+            }
+            return deque1.isEmpty() || deque2.isEmpty();
+        }
     }
 }
